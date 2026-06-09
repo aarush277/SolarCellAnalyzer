bab86aa78dc434089965e98f0a65404daa9458d8
Aarush Tripathi <aarushtripathi27@gmail.com>
2026-06-09 21:53:09 +0530

    Remove Test Title

--- solar_app.py
+++ solar_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-st.title("Solar Cell Parameter Extraction Tool - 123")
+st.title("Solar Cell Parameter Extraction Tool")
 
 # Inputs
 Area = st.number_input("Area (cm²)", value=0.0078)
