72c701e17d353e7d1c725bf55420571aa6c195a4
aarush277 <85934340+aarush277@users.noreply.github.com>
2026-06-09 21:57:11 +0530

    Update solar_app.py

--- solar_app.py
+++ solar_app.py
@@ -7,7 +7,7 @@ st.title("Solar Cell Parameter Extraction Tool")
 
 # Inputs
 Area = st.number_input("Area (cm²)", value=0.0078)
-Astar = st.number_input("A*", value=120.0)
+Astar = st.number_input("A*", value=130.0)
 T = st.number_input("Temperature (K)", value=300.0)
 
 uploaded_file = st.file_uploader(
